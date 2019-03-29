8125d015499741e4023a9a6f157183e751f6e926
Nikita Kuvykin <arrayofchar@inbox.ru>
2019-03-29 11:54:19 +0500

    - add gallery one page - fix markup gallery page - update tree

--- dev/js/building/script.js
+++ dev/js/building/script.js
@@ -6,15 +6,9 @@ import Index from './modules/index';
 import Burger from './modules/burger';
 // import Contacts from './modules/contacts';
 import Forms from './modules/forms';
-// import Share from './modules/share';
 import Sticky from './modules/sticky';
-// import Realized from './modules/realized';
-// import Filter from './modules/filter';
-// import Slider from './modules/slider';
-// import Partners from './modules/partners';
 
 document.addEventListener('DOMContentLoaded', () => {
-
   const headerEl = qs('.js-header');
   window.addEventListener('scroll', () => {
     if (window.scrollY > window.innerHeight / 3) {
@@ -40,59 +34,23 @@ document.addEventListener('DOMContentLoaded', () => {
     );
   }
 
-  // if (qs('.js-realized-elem')) {
-  //   const realized = new Realized('.js-realized-elem');
-  // }
-
   // if (qs('.js-contacts-map')) {
   //   const contacts = new Contacts('map');
   //   contacts.init();
   // }
 
+  if (qs('.js-gallery')) {
+    lightGallery(qs('.js-gallery'), {
+      selector: '.gallery__elem-href',
+      thumbnails: true,
+    });
+  }
+
   if (qsAll('form').length) Forms.init();
-
-  // if (qsAll('.js-share').length) {
-  //   qsAll('.js-share').forEach((shBtn) => {
-  //     shBtn.addEventListener('click', (e) => {
-  //       e.preventDefault();
-  //     });
-  //   });
-  //   window.share = new Share();
-  // }
-
-  // if (qsAll('.js-typecar')) {
-  //   const typeCar = new Swiper('.js-typecar', {
-  //     speed: 800,
-  //     slidesPerView: 1,
-  //     spaceBetween: 10,
-  //     loop: true,
-  //     autoHeight: true,
-  //     navigation: {
-  //       nextEl: '.js-typecar .swiper-button-next',
-  //       prevEl: '.js-typecar .swiper-button-prev',
-  //     },
-  //   });
-  // }
 
   if (qs('.js-sticky')) {
     const sticky = new Sticky();
   }
-
-  // if (qs('.js-filter')) {
-  //   const filter = new Filter('.js-filter', '.js-filter-open');
-  // }
-
-  // if (qs('.js-slider')) {
-  //   const slider = new Slider('.js-slider', '.js-slider-thumbs');
-  // }
-
-  // if (qs('.js-partners-carousel')) {
-  //   const partners = new Partners('.js-partners-carousel', '.js-partners-prev', '.js-partners-next');
-  // }
-
-  // if (qs('.js-company-paper')) {
-  //   qs('.js-company-paper').addEventListener('click', () => qs('.js-company-openpaper').click());
-  // }
 
   window.onload = () => {
     document.body.classList.add('load');
